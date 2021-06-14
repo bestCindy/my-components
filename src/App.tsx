0088ac704e0578dc8a2d056b92b3120883035b6a
wangxinyu <wangxinyu@za-doctor.com>
2021-06-14 23:57:16 +0800

    add button component part01

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
+        <Button>Hello</Button>
+        <Button disabled>Disabled Button</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">Baidu Link</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>Disabled Link</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
