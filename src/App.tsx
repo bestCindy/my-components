cc081d8bfae0fa405a905916a24835a3d6002d02
wangxinyu <wangxinyu@za-doctor.com>
2021-06-24 00:21:37 +0800

    add Menu component part-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0}>
+          <MenuItem>
+            cool link
+          </MenuItem>
+          <MenuItem>
+            cool link 2
+          </MenuItem>
+          <MenuItem>
+            cool link 3
+          </MenuItem>
+        </Menu>
+
         <Button autoFocus>Hello</Button>
         <Button disabled>Disabled Button</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
